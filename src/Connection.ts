5a7e5f1323eccb9620ad935c84d9d5ed6a1803cc
tychota <tychota@users.noreply.github.com>
2019-10-06 22:49:37 +0200

    chore: reformat code using prettier

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,18 +1,18 @@
 import * as amqp from "amqplib";
 import DevNullLogger from "./DevNullLogger";
-import {ILogger} from "./ILogger";
+import { ILogger } from "./ILogger";
 
 const WAIT_MS: number = 2000;
 const WAIT_MAX_MS: number = 300000; // 5 * 60 * 1000 = 5 min
 
 export interface IConnectionOptions {
-    connectionString?: string;
-    user?: string;
-    pass?: string;
-    host?: string;
-    port?: number;
-    vhost?: string;
-    heartbeat?: number;
+  connectionString?: string;
+  user?: string;
+  pass?: string;
+  host?: string;
+  port?: number;
+  vhost?: string;
+  heartbeat?: number;
 }
 
 export type createChannelCallback = (ch: amqp.Channel) => Promise<void>;
@@ -21,142 +21,156 @@ export type createChannelCallback = (ch: amqp.Channel) => Promise<void>;
  * Logical wrap for AMQP connection
  */
 export class Connection {
+  private connStr: string;
+  private connection: Promise<amqp.Connection>;
+  private recreateConnection: boolean = true;
 
-    private connStr: string;
-    private connection: Promise<amqp.Connection>;
-    private recreateConnection: boolean = true;
-
-    /**
-     *
-     * @param {IConnectionOptions} opts
-     * @param {ILogger} logger
-     */
-    constructor(opts: IConnectionOptions, private logger?: ILogger) {
-        if (opts.connectionString) {
-            this.connStr = opts.connectionString;
-        } else {
-            this.connStr = `amqp://${opts.user}:${opts.pass}@${opts.host}:${opts.port}${opts.vhost || "/"}`;
-            this.connStr += `?heartbeat=${opts.heartbeat || 60}`;
-        }
-
-        if (!logger) {
-            this.logger = new DevNullLogger();
-        }
-
-        this.connection = this.createConnection();
+  /**
+   *
+   * @param {IConnectionOptions} opts
+   * @param {ILogger} logger
+   */
+  constructor(opts: IConnectionOptions, private logger?: ILogger) {
+    if (opts.connectionString) {
+      this.connStr = opts.connectionString;
+    } else {
+      this.connStr = `amqp://${opts.user}:${opts.pass}@${opts.host}:${
+        opts.port
+      }${opts.vhost || "/"}`;
+      this.connStr += `?heartbeat=${opts.heartbeat || 60}`;
     }
 
-    /**
-     * Returns promise with current amqp connection
-     */
-    public connect(): Promise<amqp.Connection> {
-        return this.connection;
+    if (!logger) {
+      this.logger = new DevNullLogger();
     }
 
-    /**
-     * Retursn the actal connection string being used
-     */
-    public getConnectionString(): string {
-        return this.connStr;
-    }
+    this.connection = this.createConnection();
+  }
 
-    /**
-     * Sets whether the connection should be automatically recreated when closed
-     * @param should
-     */
-    public shouldRecreateConnection(should: boolean) {
-        this.recreateConnection = should;
-    }
+  /**
+   * Returns promise with current amqp connection
+   */
+  public connect(): Promise<amqp.Connection> {
+    return this.connection;
+  }
 
-    /**
-     * Closes the connection
-     * Note: forces this.recreateChannel to be false
-     */
-    public async close(): Promise<void> {
-        this.shouldRecreateConnection(false);
-        const conn = await this.connection;
-        await conn.close();
-    }
+  /**
+   * Retursn the actal connection string being used
+   */
+  public getConnectionString(): string {
+    return this.connStr;
+  }
 
-    /**
-     * Returns promise of channel with applied functions on the channel
-     */
-    public createChannel(
-        prepareFn: createChannelCallback,
-        useConfirmChannel = true,
-    ): Promise<amqp.Channel|amqp.ConfirmChannel> {
-        return new Promise((resolve) => {
-            let tryCount: number = 1;
+  /**
+   * Sets whether the connection should be automatically recreated when closed
+   * @param should
+   */
+  public shouldRecreateConnection(should: boolean) {
+    this.recreateConnection = should;
+  }
 
-            const reconnect: (reason: any) => void = (reason) => {
-                const wait: number = Math.min(WAIT_MS * tryCount, WAIT_MAX_MS); // wait max 5 min
+  /**
+   * Closes the connection
+   * Note: forces this.recreateChannel to be false
+   */
+  public async close(): Promise<void> {
+    this.shouldRecreateConnection(false);
+    const conn = await this.connection;
+    await conn.close();
+  }
 
-                this.logger.error(`Channel creation failed. Retry after ${wait} ms. Reason: ${reason}`);
+  /**
+   * Returns promise of channel with applied functions on the channel
+   */
+  public createChannel(
+    prepareFn: createChannelCallback,
+    useConfirmChannel = true
+  ): Promise<amqp.Channel | amqp.ConfirmChannel> {
+    return new Promise(resolve => {
+      let tryCount: number = 1;
 
-                setTimeout(tryConnect, wait);
-                tryCount += 1;
-            };
+      const reconnect: (reason: any) => void = reason => {
+        const wait: number = Math.min(WAIT_MS * tryCount, WAIT_MAX_MS); // wait max 5 min
 
-            let channel: amqp.Channel;
-            const tryConnect: () => void = () => this.connection
-                .then((connection) => {
-                    if (useConfirmChannel) {
-                        return connection.createConfirmChannel();
-                    }
-                    return connection.createChannel();
-                })
-                .then((ch: amqp.Channel) => {
-                    channel = ch;
-                    return prepareFn(ch);
-                })
-                .then(() => {
-                    return resolve(channel);
-                })
-                .catch(reconnect);
+        this.logger.error(
+          `Channel creation failed. Retry after ${wait} ms. Reason: ${reason}`
+        );
 
-            tryConnect();
-        });
-    }
+        setTimeout(tryConnect, wait);
+        tryCount += 1;
+      };
 
-    /**
-     * Creates new connection promise to RabbitMQ
-     */
-    private createConnection(): Promise<amqp.Connection> {
-        return new Promise((resolve/* , reject*/) => {
-            let tryCount = 1;
+      let channel: amqp.Channel;
+      const tryConnect: () => void = () =>
+        this.connection
+          .then(connection => {
+            if (useConfirmChannel) {
+              return connection.createConfirmChannel();
+            }
+            return connection.createChannel();
+          })
+          .then((ch: amqp.Channel) => {
+            channel = ch;
+            return prepareFn(ch);
+          })
+          .then(() => {
+            return resolve(channel);
+          })
+          .catch(reconnect);
 
-            const reconnect: (error: any) => void = (error) => {
-                /* try forever
+      tryConnect();
+    });
+  }
+
+  /**
+   * Creates new connection promise to RabbitMQ
+   */
+  private createConnection(): Promise<amqp.Connection> {
+    return new Promise((resolve /* , reject*/) => {
+      let tryCount = 1;
+
+      const reconnect: (error: any) => void = error => {
+        /* try forever
                 if (tryCount >= (WAIT_MAX_MS / WAIT_MS) + WAIT_LIMIT) { // 5 min + 2 h
                   reject(`Maximum (${tryCount}) reconnection attempts reached.`);
                 }*/
 
-                const wait: number = Math.min(WAIT_MS * tryCount, WAIT_MAX_MS); // wait max 5 min
+        const wait: number = Math.min(WAIT_MS * tryCount, WAIT_MAX_MS); // wait max 5 min
 
-                this.logger.error(`RabbitMQ connection failure. Retry after ${wait} ms. Reason: ${error.message}`);
+        this.logger.error(
+          `RabbitMQ connection failure. Retry after ${wait} ms. Reason: ${error.message}`
+        );
 
-                setTimeout(tryConnect, wait);
-                tryCount += 1;
-            };
+        setTimeout(tryConnect, wait);
+        tryCount += 1;
+      };
 
-            const tryConnect: () => void = () => amqp.connect(this.connStr)
-                .then((connection) => {
-                    connection.on("close", (error) => {
-                        this.logger.warn("AMQP Connection closed", error ? error.message : "");
-                        if (this.recreateConnection) {
-                            this.connection = this.createConnection();
-                        }
-                    });
-                    connection.on("error", (error) => {
-                        this.logger.error("AMQP Connection error", error ? error.message : "");
-                        // will be handled by close event
-                    });
-                    this.logger.info("Connected to RabbitMQ.");
-                    resolve(connection);
-                })
-                .catch(reconnect);
+      const tryConnect: () => void = () =>
+        amqp
+          .connect(this.connStr)
+          .then(connection => {
+            connection.on("close", error => {
+              this.logger.warn(
+                "AMQP Connection closed",
+                error ? error.message : ""
+              );
+              if (this.recreateConnection) {
+                this.connection = this.createConnection();
+              }
+            });
+            connection.on("error", error => {
+              this.logger.error(
+                "AMQP Connection error",
+                error ? error.message : ""
+              );
+              // will be handled by close event
+            });
+            this.logger.info("Connected to RabbitMQ.");
+            resolve(connection);
+          })
+          .catch(reconnect);
 
-            tryConnect();
-        });
-    }
+      tryConnect();
+    });
+  }
 }
--- src/ILogger.ts
+++ src/ILogger.ts
@@ -1,8 +1,6 @@
 export interface ILogger {
-
-    log(message?: any, ...optionalParams: any[]): void;
-    info(message?: any, ...optionalParams: any[]): void;
-    warn(message?: any, ...optionalParams: any[]): void;
-    error(message?: any, ...optionalParams: any[]): void;
-
+  log(message?: any, ...optionalParams: any[]): void;
+  info(message?: any, ...optionalParams: any[]): void;
+  warn(message?: any, ...optionalParams: any[]): void;
+  error(message?: any, ...optionalParams: any[]): void;
 }
