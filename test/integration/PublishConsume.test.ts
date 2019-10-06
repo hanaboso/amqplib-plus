f3ecc1e1254cfec74503f0d4ce86fd8c8de5d71e
tychota <tychota@users.noreply.github.com>
2019-10-06 22:48:15 +0200

    fix: fix paths

--- test/integration/PublishConsume.test.ts
+++ test/integration/PublishConsume.test.ts
@@ -1,6 +1,8 @@
 import { Channel, Message } from "amqplib";
-import { Connection } from "../../lib/Connection";
-import { Publisher } from "../../lib/Publisher";
+
+import { Connection } from "@src/Connection";
+import { Publisher } from "@src/Publisher";
+
 import { SimpleConsumer } from "../common/consumer/SimpleConsumer";
 import { rabbitMQOptions } from "../config";
 
@@ -10,113 +12,111 @@ import "mocha";
 const conn = new Connection(rabbitMQOptions);
 
 describe("Publish Consume", () => {
+  it("should publish and consume single message", done => {
+    const testQueue = {
+      name: "test_queue_single",
+      options: {}
+    };
+    const testContent = "test content";
 
-    it("should publish and consume single message", (done) => {
-        const testQueue = {
-            name: "test_queue_single",
-            options: {},
-        };
-        const testContent = "test content";
+    const publisherPrepare: any = (ch: Channel) => {
+      return ch.assertQueue(testQueue.name, testQueue.options);
+    };
+    const consumerPrepare: any = (ch: Channel) => {
+      return ch.assertQueue(testQueue.name, testQueue.options);
+    };
 
-        const publisherPrepare: any = (ch: Channel) => {
-            return ch.assertQueue(testQueue.name, testQueue.options);
-        };
-        const consumerPrepare: any = (ch: Channel) => {
-            return ch.assertQueue(testQueue.name, testQueue.options);
-        };
+    const publisher = new Publisher(conn, publisherPrepare);
+    const assertFn = (msg: Message) => {
+      assert.equal(msg.content.toString(), testContent);
+      done();
+    };
+    const consumer = new SimpleConsumer(conn, consumerPrepare, assertFn);
 
-        const publisher = new Publisher(conn, publisherPrepare);
-        const assertFn = (msg: Message) => {
-            assert.equal(msg.content.toString(), testContent);
-            done();
-        };
-        const consumer = new SimpleConsumer(conn, consumerPrepare, assertFn);
+    consumer.consume(testQueue.name, {});
+    publisher.sendToQueue(testQueue.name, Buffer.from(testContent), {});
+  });
 
-        consumer.consume(testQueue.name, {});
-        publisher.sendToQueue(testQueue.name, Buffer.from(testContent), {});
-    });
+  it("should publish and consume single message using confirm channel", done => {
+    const useConfirmChannel = true;
+    const testQueue = {
+      name: "test_queue_single_confirm",
+      options: {}
+    };
+    const testContent = "test content";
 
-    it("should publish and consume single message using confirm channel", (done) => {
-        const useConfirmChannel = true;
-        const testQueue = {
-            name: "test_queue_single_confirm",
-            options: {},
-        };
-        const testContent = "test content";
+    const publisherPrepare: any = (ch: Channel) => {
+      return ch.assertQueue(testQueue.name, testQueue.options);
+    };
+    const consumerPrepare: any = (ch: Channel) => {
+      return ch.assertQueue(testQueue.name, testQueue.options);
+    };
 
-        const publisherPrepare: any = (ch: Channel) => {
-            return ch.assertQueue(testQueue.name, testQueue.options);
-        };
-        const consumerPrepare: any = (ch: Channel) => {
-            return ch.assertQueue(testQueue.name, testQueue.options);
-        };
+    const publisher = new Publisher(conn, publisherPrepare, useConfirmChannel);
+    const assertFn = (msg: Message) => {
+      assert.equal(msg.content.toString(), testContent);
+      done();
+    };
+    const consumer = new SimpleConsumer(conn, consumerPrepare, assertFn);
 
-        const publisher = new Publisher(conn, publisherPrepare, useConfirmChannel);
-        const assertFn = (msg: Message) => {
-            assert.equal(msg.content.toString(), testContent);
-            done();
-        };
-        const consumer = new SimpleConsumer(conn, consumerPrepare, assertFn);
+    consumer.consume(testQueue.name, {});
+    publisher.sendToQueue(testQueue.name, Buffer.from(testContent), {});
+  });
 
-        consumer.consume(testQueue.name, {});
-        publisher.sendToQueue(testQueue.name, Buffer.from(testContent), {});
-    });
+  it("should publish and consume multiple messages", done => {
+    let msgReceived = 0;
+    const msgSent = 5;
+    const testQueue = {
+      name: "test_queue_multiple",
+      options: {}
+    };
 
-    it("should publish and consume multiple messages", (done) => {
-        let msgReceived = 0;
-        const msgSent = 5;
-        const testQueue = {
-            name: "test_queue_multiple",
-            options: {},
-        };
+    const publisherPrepare: any = (ch: Channel) => {
+      return ch.assertQueue(testQueue.name, testQueue.options);
+    };
+    const consumerPrepare: any = (ch: Channel) => {
+      return ch.assertQueue(testQueue.name, testQueue.options);
+    };
 
-        const publisherPrepare: any = (ch: Channel) => {
-            return ch.assertQueue(testQueue.name, testQueue.options);
-        };
-        const consumerPrepare: any = (ch: Channel) => {
-            return ch.assertQueue(testQueue.name, testQueue.options);
-        };
+    const publisher = new Publisher(conn, publisherPrepare);
+    const assertFn = (msg: Message) => {
+      assert.equal(msg.content.toString(), "some content");
+      msgReceived++;
+      if (msgReceived === msgSent) {
+        done();
+      }
+    };
+    const consumer = new SimpleConsumer(conn, consumerPrepare, assertFn);
 
-        const publisher = new Publisher(conn, publisherPrepare);
-        const assertFn = (msg: Message) => {
-            assert.equal(msg.content.toString(), "some content");
-            msgReceived++;
-            if (msgReceived === msgSent) {
-                done();
-            }
-        };
-        const consumer = new SimpleConsumer(conn, consumerPrepare, assertFn);
+    consumer.consume(testQueue.name, {});
+    for (let i = 0; i < msgSent; i++) {
+      publisher.sendToQueue(testQueue.name, Buffer.from("some content"), {});
+    }
+  });
 
-        consumer.consume(testQueue.name, {});
-        for (let i = 0; i < msgSent; i++) {
-            publisher.sendToQueue(testQueue.name, Buffer.from("some content"), {});
-        }
-    });
+  it("publish is able to publish many messages", done => {
+    const msgSent = 10000;
+    const testQueue = {
+      name: "test_queue_publish_many",
+      options: {}
+    };
 
-    it("publish is able to publish many messages", (done) => {
-        const msgSent = 10000;
-        const testQueue = {
-            name: "test_queue_publish_many",
-            options: {},
-        };
+    let channel: Channel;
+    const publisherPrepare: any = async (ch: Channel) => {
+      channel = ch;
+      await ch.assertQueue(testQueue.name, testQueue.options);
+      await ch.purgeQueue(testQueue.name);
+    };
 
-        let channel: Channel;
-        const publisherPrepare: any = async (ch: Channel) => {
-            channel = ch;
-            await ch.assertQueue(testQueue.name, testQueue.options);
-            await ch.purgeQueue(testQueue.name);
-        };
+    const publisher = new Publisher(conn, publisherPrepare);
+    for (let i = 0; i < msgSent; i++) {
+      publisher.sendToQueue(testQueue.name, Buffer.from("some content"), {});
+    }
 
-        const publisher = new Publisher(conn, publisherPrepare);
-        for (let i = 0; i < msgSent; i++) {
-            publisher.sendToQueue(testQueue.name, Buffer.from("some content"), {});
-        }
-
-        setTimeout(async () => {
-            const info = await channel.checkQueue(testQueue.name);
-            assert.equal(info.messageCount, msgSent);
-            done();
-        }, 10000);
-    }).timeout(11000);
-
+    setTimeout(async () => {
+      const info = await channel.checkQueue(testQueue.name);
+      assert.equal(info.messageCount, msgSent);
+      done();
+    }, 10000);
+  }).timeout(11000);
 });
--- test/unit/Connection.test.ts
+++ test/unit/Connection.test.ts
@@ -1,23 +1,35 @@
-import { Connection } from "../../lib/Connection";
+import { Connection } from "@src/Connection";
 
 import { assert } from "chai";
 import "mocha";
 
 describe("Connection", () => {
-
-    it("should accept connection string", async () => {
-        const conn = new Connection({connectionString: "amqp://rabbit:5672/root"});
-        assert.equal(conn.getConnectionString(), "amqp://rabbit:5672/root");
+  it("should accept connection string", async () => {
+    const conn = new Connection({
+      connectionString: "amqp://rabbit:5672/root"
     });
+    assert.equal(conn.getConnectionString(), "amqp://rabbit:5672/root");
+  });
 
-    it("should accept connection options", async () => {
-        const conn = new Connection({host: "rabbit", port: 5672, pass: "guest", user: "guest"});
-        assert.equal(conn.getConnectionString(), "amqp://guest:guest@rabbit:5672/?heartbeat=60");
+  it("should accept connection options", async () => {
+    const conn = new Connection({
+      host: "rabbit",
+      port: 5672,
+      pass: "guest",
+      user: "guest"
     });
+    assert.equal(
+      conn.getConnectionString(),
+      "amqp://guest:guest@rabbit:5672/?heartbeat=60"
+    );
+  });
 
-    it("should prefer connection string over options options", async () => {
-        const conn = new Connection({host: "rabbit", port: 5672, connectionString: "amqp://rabbit:5672/root"});
-        assert.equal(conn.getConnectionString(), "amqp://rabbit:5672/root");
+  it("should prefer connection string over options options", async () => {
+    const conn = new Connection({
+      host: "rabbit",
+      port: 5672,
+      connectionString: "amqp://rabbit:5672/root"
     });
-
+    assert.equal(conn.getConnectionString(), "amqp://rabbit:5672/root");
+  });
 });
