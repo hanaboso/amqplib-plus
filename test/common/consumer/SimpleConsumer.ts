db3ff54f11218c9d31a6c200e6f9811f1f91444f
tychota <tychota@users.noreply.github.com>
2019-10-06 22:48:15 +0200

    fix: fix paths

--- test/common/consumer/SimpleConsumer.ts
+++ test/common/consumer/SimpleConsumer.ts
@@ -1,6 +1,7 @@
 import { Channel, Message } from "amqplib";
-import { Connection } from "../../../lib/Connection";
-import { Consumer } from "../../../lib/Consumer";
+
+import { Connection } from "@src/Connection";
+import { Consumer } from "@src/Consumer";
 
 /**
  * Example implementation of Consumer
@@ -10,37 +11,35 @@ import { Consumer } from "../../../lib/Consumer";
  * Simple consumers automatically acks amqp message when consumed
  */
 export class SimpleConsumer extends Consumer {
+  private processData: (msg: Message) => void;
 
-    private processData: (msg: Message) => void;
+  /**
+   *
+   * @param {Connection} conn
+   * @param {(ch: Channel) => Promise<void>} channelCb
+   * @param {(msg: Message) => void} processData
+   */
+  constructor(
+    conn: Connection,
+    channelCb: (ch: Channel) => Promise<void>,
+    processData: (msg: Message) => void
+  ) {
+    super(conn, channelCb);
+    this.processData = processData;
+  }
 
-    /**
-     *
-     * @param {Connection} conn
-     * @param {(ch: Channel) => Promise<void>} channelCb
-     * @param {(msg: Message) => void} processData
-     */
-    constructor(
-        conn: Connection,
-        channelCb: (ch: Channel) => Promise<void>,
-        processData: (msg: Message) => void,
-    ) {
-        super(conn, channelCb);
-        this.processData = processData;
+  /**
+   * Passes message to defined process function and acks amqp message
+   *
+   * @param {Message} msg
+   * @param {Channel} channel
+   */
+  public processMessage(msg: Message, channel: Channel): void {
+    try {
+      this.processData(msg);
+      channel.ack(msg);
+    } catch (e) {
+      channel.reject(msg);
     }
-
-    /**
-     * Passes message to defined process function and acks amqp message
-     *
-     * @param {Message} msg
-     * @param {Channel} channel
-     */
-    public processMessage(msg: Message, channel: Channel): void {
-        try {
-            this.processData(msg);
-            channel.ack(msg);
-        } catch (e) {
-            channel.reject(msg);
-        }
-    }
-
+  }
 }
--- test/unit/Consumer.test.ts
+++ test/unit/Consumer.test.ts
@@ -1,6 +1,8 @@
 import { Replies } from "amqplib/properties";
-import { Connection } from "../../lib/Connection";
-import { Consumer } from "../../lib/Consumer";
+
+import { Connection } from "@src/Connection";
+import { Consumer } from "@src/Consumer";
+
 import { SimpleConsumer } from "../common/consumer/SimpleConsumer";
 import { rabbitMQOptions } from "../config";
 
@@ -10,33 +12,35 @@ import "mocha";
 const conn = new Connection(rabbitMQOptions);
 
 describe("Consumer", () => {
+  it("should call channel's consume and cancel methods", async () => {
+    let consumeCalled = false;
+    let cancelCalled = false;
 
-    it("should call channel's consume and cancel methods", async () => {
-        let consumeCalled = false;
-        let cancelCalled = false;
+    const channelMock: any = {
+      consume: (): Promise<Replies.Consume> => {
+        consumeCalled = true;
 
-        const channelMock: any = {
-            consume: (): Promise<Replies.Consume> => {
-                consumeCalled = true;
+        return Promise.resolve({ consumerTag: "consumerTagValue" });
+      },
+      cancel: (consumerTag: string): Promise<void> => {
+        assert.equal(consumerTag, "consumerTagValue");
+        cancelCalled = true;
 
-                return Promise.resolve({consumerTag: "consumerTagValue"});
-            },
-            cancel: (consumerTag: string): Promise<void> => {
-                assert.equal(consumerTag, "consumerTagValue");
-                cancelCalled = true;
+        return Promise.resolve();
+      },
+      on: () => false
+    };
+    conn.createChannel = async () => channelMock;
+    const consumer = new SimpleConsumer(
+      conn,
+      () => Promise.resolve(),
+      () => null
+    );
 
-                return Promise.resolve();
-            },
-            on: () => false,
-        };
-        conn.createChannel = async () => channelMock;
-        const consumer = new SimpleConsumer(conn, () => Promise.resolve(), () => null);
+    const tag = await consumer.consume("queue-name", {});
+    await consumer.cancel(tag);
 
-        const tag = await consumer.consume("queue-name", {});
-        await consumer.cancel(tag);
-
-        assert.isTrue(consumeCalled);
-        assert.isTrue(cancelCalled);
-    });
-
+    assert.isTrue(consumeCalled);
+    assert.isTrue(cancelCalled);
+  });
 });
